5928c5346aaffef5943acc3e652324b57c4e8af8
thaisamorandini89 <thaisamorandini@gmail.com>
2023-04-19 10:59:13 -0300

    Mudanças pontuais

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 'http://172.21.200.90:3000';
 
-export const makeRequest = ({method = 'GET', url, data, params}) => {
+const makeRequest = ({method = 'GET', url, data, params}) => {
   return axios ({
       method,
       url: `${BASE_URL}${url}` ,
